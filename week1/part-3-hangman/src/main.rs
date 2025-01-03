5047f51068977fb37acc9266656eddc7b7589b13
Zane_Liao <lzq666amn@gmail.com>
2025-01-03 22:08:19 +0800

    two

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -19,7 +19,6 @@ use std::fs;
 use std::io;
 use std::io::Write;
 
-const NUM_INCORRECT_GUESSES: u32 = 5;
 const WORDS_PATH: &str = "words.txt";
 
 fn pick_a_random_word() -> String {
@@ -29,56 +28,77 @@ fn pick_a_random_word() -> String {
 }
 
 fn main() {
+    let mut  num_incorrect_guesses: u32 = 5;
     let secret_word = pick_a_random_word();
     // Note: given what you know about Rust so far, it's easier to pull characters out of a
     // vector than it is to pull them out of a string. You can get the ith character of
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
-    // Uncomment for debugging:
-    // Your code here! :)
-    println!("Welcome to CS110L Hangman!");
-
+        // Uncomment for debugging:
+        // Your code here! :)
+        println!("Welcome to CS110L Hangman!");
+        println!("The word so far is: ");
+        println!("You have guessed the following letters: ");
+        println!("You have {} guesses left ", num_incorrect_guesses);
+        println!("Please guess a letter: {:?}", secret_word_chars.iter().next());
     loop {
         let mut guess = String::new();
-        
+
+        io::stdout()
+        .flush()
+        .expect("Error flushing stdout.");
+
         io::stdin()
         .read_line(&mut guess)
         .expect("");
 
-        println!("The word so far is: {}", sequence_with_char(&secret_word, &guess));
-        println!("You have guessed the following letters: {}", guess);
-        
-        println!("Please guess a letter: {:?}", secret_word_chars.iter().next());
+        let guess = Some(guess.chars().next().unwrap());
 
-        println!("random word: {}", secret_word);
-
-        if secret_word == guess {
-            println!("You have {} guesses left ", NUM_INCORRECT_GUESSES-1);
-        } else if NUM_INCORRECT_GUESSES < 1 && secret_word != guess{
-            println!("Sorry, you ran out of guesses!");
-            break;
-        } else {
-            println!("Sorry, that letter is not in the word");
-        }
-
-        let guess = guess.chars().next().unwrap();
-
-
-        match guess.cmp(&secret_word_chars.iter().next().unwrap()) {
-            cmp::Ordering::Less    => println!("Too small!"),
-            cmp::Ordering::Greater => println!("Too big!"),
-            cmp::Ordering::Equal   => {
-                println!("Congratulations you guessed the secret word: {}", secret_word);
-                break;
+        if let Some(letter) = guess {
+            for c in &secret_word_chars { 
+                    if *c == letter {
+                        println!("The word so far is: {}", sequence_with_char(&secret_word.chars().next().unwrap(), &guess));
+                        println!("You have guessed the following letters: {:?}", guess);
+                        println!("You have {} guesses left ", num_incorrect_guesses);
+                        println!("Please guess a letter: {:?}", secret_word_chars.iter().next());
+                    } 
             }
+            println!("Sorry, that letter is not in the word");
+            num_incorrect_guesses -= 1;
+            if num_incorrect_guesses < 1 {
+                println!("Sorry, you ran out of guesses!");
+                    break;
+            }            
         }
+
+        
+
+
+        //     else if NUM_INCORRECT_GUESSES < 1 && secret_word != guess{
+            // println!("Sorry, you ran out of guesses!");
+            // break;
+        // } else {
+            // println!("Sorry, that letter is not in the word");
+        // }
+
+
+
+
+        // match guess.cmp(&secret_word_chars.iter().next().unwrap()) {
+        //     cmp::Ordering::Less    => println!("Too small!"),
+        //     cmp::Ordering::Greater => println!("Too big!"),
+        //     cmp::Ordering::Equal   => {
+        //         println!("Congratulations you guessed the secret word: {}", secret_word);
+        //         break;
+        //     }
+        // }
     }
 }
 
-fn sequence_with_char(s: &str, c: &String) -> String {
-    let mut result = "-".repeat(s.len());
-    let mid    = s.len() / 2;
+fn sequence_with_char(s: &char, c: &String) -> String {
+    let mut result = "-".repeat(s.to_string().len());
+    let mid= s.to_string().len() / 2;
 
-    result.replace_range(mid..mid+1, &c);
-    result
+    result.replace_range(mid..mid+1, &c.to_string());
+    result.to_string()
 }
